7c09e3a60d20e5eec5206f5726d258f2a072bb34
Carlos Allende <carlosallendesanz@gmail.com>
2016-06-29 19:32:42 -0700

    ui.routes and templates working

--- js/config/routes.js
+++ js/config/routes.js
@@ -8,31 +8,23 @@
     $stateProvider
     .state ('home', {
       url: '/',
-      templateUrl: 'index.html'
+      templateUrl: 'js/home/home.html'
     })
     .state ('About', {
       url: '/About',
-      templateUrl: 'templates/About.html'
+      templateUrl: 'js/about/about.html'
     })
     .state ('Work', {
       url: '/Work',
-      templateUrl: 'templates/Work.html'
+      templateUrl: 'js/work/work.html'
     })
     .state ('Service', {
       url: '/Service',
-      templateUrl: 'templates/Service.html'
+      templateUrl: 'js/service/service.html'
     })
     .state ('Contact', {
       url: '/Contact',
-      templateUrl: 'templates/Contact.html'
-    })
-    .state ('hiring_employer', {
-      url: '/hiring_employer',
-      templateUrl: 'templates/hiring_employer.html'
-    })
-    .state ('prospect_client', {
-      url: '/prospect_client',
-      templateUrl: 'templates/prospect_client.html'
+      templateUrl: 'js/contact/contact.html'
     })
     $urlRouterProvider.otherwise('/');
   }
